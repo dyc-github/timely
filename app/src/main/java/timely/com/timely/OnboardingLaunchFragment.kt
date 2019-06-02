d25e6121c8eb1796c9433a11464579064145ca67
Kevin Seo <kseo@SFOC02WG14UHTD7.sea.corp.expecn.com>
2019-06-01 17:28:13 -0700

    change name of login fragment to onboarding launch fragment

--- app/src/main/java/timely/com/timely/OnboardingLaunchFragment.kt
+++ app/src/main/java/timely/com/timely/OnboardingLaunchFragment.kt
@@ -6,18 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_onbaording_launch.*
 
-class LoginFragment : Fragment() {
+class OnboardingLaunchFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_onbaording_launch, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         sign_up_button.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+            findNavController().navigate(R.id.action_onboardingLaunchFragment_to_signUpFragment)
         }
     }
 }
