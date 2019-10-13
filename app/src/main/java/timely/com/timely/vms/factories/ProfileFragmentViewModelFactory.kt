8fafd42eacdd28dcf12159bc603aadb15df56878
Kevin Seo <kseo@SFOC02WG14UHTD7.sea.corp.expecn.com>
2019-10-12 17:19:22 -0700

    fixed_user thing

--- app/src/main/java/timely/com/timely/vms/factories/ProfileFragmentViewModelFactory.kt
+++ app/src/main/java/timely/com/timely/vms/factories/ProfileFragmentViewModelFactory.kt
@@ -1,15 +1,17 @@
 package timely.com.timely.vms.factories
 
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.auth.FirebaseAuth
 import timely.com.timely.helpers.FirestoreService
+import timely.com.timely.repositories.UserRepository
 import timely.com.timely.vms.ProfileFragmentViewModelImpl
 import javax.inject.Inject
 
 @Suppress("UNCHECKED_CAST")
-class ProfileFragmentViewModelFactory @Inject constructor(private val firestoreService: FirestoreService, private val firebaseAuth: FirebaseAuth) : ViewModelProvider.Factory {
+class ProfileFragmentViewModelFactory @Inject constructor(private val activity: AppCompatActivity, private val firestoreService: FirestoreService, private val firebaseAuth: FirebaseAuth, private val userRepository: UserRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return ProfileFragmentViewModelImpl(firestoreService, firebaseAuth) as T
+        return ProfileFragmentViewModelImpl(activity, firestoreService, firebaseAuth, userRepository) as T
     }
 }
